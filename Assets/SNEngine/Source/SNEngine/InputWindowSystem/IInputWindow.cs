73427bbda448491361d83e14af7ef4c23c265b26
Maxim <36982547+Siphoin@users.noreply.github.com>
2025-12-04 10:25:34 +0500

    add Input Window

---
+++ Assets/SNEngine/Source/SNEngine/InputWindowSystem/IInputWindow.cs
@@ -0,0 +1,12 @@
+﻿using Cysharp.Threading.Tasks;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace SNEngine.InputWindowSystem
+{
+    public interface IInputWindow : IResetable, IShowable, IHidden
+    {
+        void SetData(string keyTitle, Sprite icon);
+         UniTask<InputWindowResult> WaitInputPlayer();
+    }
+}
---
+++ Assets/SNEngine/Source/SNEngine/InputWindowSystem/InputWindow.cs
@@ -0,0 +1,62 @@
+﻿using Cysharp.Threading.Tasks;
+using SNEngine.Extensions;
+using SNEngine.Localization;
+using SNEngine.Services;
+using System.Collections;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace SNEngine.InputWindowSystem
+{
+    public class InputWindow : MonoBehaviour, IInputWindow
+    {
+        [SerializeField] private TMP_InputField _input;
+        [SerializeField] private Image _icon;
+        [SerializeField] private UILocalizationText _title;
+        [SerializeField] private Button _confirmButton;
+        [SerializeField] private Button _cancelButton;
+
+
+
+        public void SetData(string keyTitle, Sprite icon)
+        {
+            _title.ChangeKey(keyTitle);
+            _icon.sprite = icon;
+            _icon.SetAdaptiveSize();
+        }
+
+        public async UniTask<InputWindowResult> WaitInputPlayer()
+        {
+            var source = new UniTaskCompletionSource<InputWindowButton>();
+
+            _confirmButton.onClick.RemoveAllListeners();
+            _cancelButton.onClick.RemoveAllListeners();
+
+            _confirmButton.onClick.AddListener(() => source.TrySetResult(InputWindowButton.Ok));
+            _cancelButton.onClick.AddListener(() => source.TrySetResult(InputWindowButton.Cancel));
+
+            InputWindowButton button = await source.Task;
+
+            return new InputWindowResult(_input.text, button);
+        }
+
+        public void Hide()
+        {
+            gameObject.SetActive(false);
+
+        }
+
+        public void Show()
+        {
+            gameObject.SetActive(true);
+        }
+
+        public void ResetState()
+        {
+            _input.text = string.Empty;
+            _confirmButton.onClick.RemoveAllListeners();
+            _cancelButton.onClick.RemoveAllListeners();
+        }
+    }
+}
