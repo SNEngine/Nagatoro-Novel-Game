259d02881aa4c140db926e1b4cfd3c40e593e442
Maxim <36982547+Siphoin@users.noreply.github.com>
2025-12-18 15:48:24 +0500

    Update VaritableContainerGraphEditor.cs

--- Assets/SNEngine/Source/XNodeExtensions/Editor/VaritableContainerGraphEditor.cs
+++ Assets/SNEngine/Source/XNodeExtensions/Editor/VaritableContainerGraphEditor.cs
@@ -1,44 +1,40 @@
 using System;
-using System.Linq;
-using UnityEditor;
-using UnityEngine;
-using XNode;
-using XNodeEditor;
-using SiphoinUnityHelpers.XNodeExtensions;
+using System.Collections.Generic;
 
 namespace SiphoinUnityHelpers.XNodeExtensions.Editor
 {
-    // Конкретная реализация для VaritableContainerGraph - фильтруем по VaritableNode и SetVaritableNode производным
     [CustomNodeGraphEditor(typeof(SNEngine.Graphs.VaritableContainerGraph))]
     public class VaritableContainerGraphEditor : FilteredNodeGraphEditor
     {
+        private static readonly Dictionary<Type, bool> _typeCache = new Dictionary<Type, bool>();
+
         protected override bool IsNodeTypeAllowed(Type nodeType)
         {
-            // Проверяем, является ли тип производным от VaritableNode
+            if (_typeCache.TryGetValue(nodeType, out bool allowed))
+                return allowed;
+
+            allowed = CheckIfAllowed(nodeType);
+            _typeCache[nodeType] = allowed;
+            return allowed;
+        }
+
+        private bool CheckIfAllowed(Type nodeType)
+        {
             if (typeof(VaritableNode).IsAssignableFrom(nodeType))
                 return true;
 
-            // Проверяем, является ли тип производным от SetVaritableNode<T> (любого типа T)
-            var currentType = nodeType;
+            Type currentType = nodeType;
             while (currentType != null && currentType != typeof(object))
             {
-                if (currentType.IsGenericType)
+                if (currentType.IsGenericType &&
+                    currentType.GetGenericTypeDefinition().Name.Contains("SetVaritableNode"))
                 {
-                    var genericTypeDef = currentType.GetGenericTypeDefinition();
-                    // Проверяем, является ли базовый тип одним из SetVaritableNode<T>
-                    if (genericTypeDef.Name.StartsWith("SetVaritableNode"))
-                    {
-                        return true;
-                    }
+                    return true;
                 }
                 currentType = currentType.BaseType;
             }
 
-            // Дополнительно проверяем, если имя класса содержит "SetVaritable" или является специфичным типом
-            if (nodeType.Name.Contains("SetVaritable"))
-                return true;
-
-            return false; // Все остальные типы запрещены
+            return nodeType.Name.Contains("SetVaritable");
         }
     }
 }
